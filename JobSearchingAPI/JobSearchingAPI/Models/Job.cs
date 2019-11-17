0790f61943800389ef665a872893f3c1125ecb63
Deva <devadcool20@gmail.com>
2019-11-17 13:36:07 -0500

    api dummy data

--- JobSearchingAPI/JobSearchingAPI/Models/Job.cs
+++ JobSearchingAPI/JobSearchingAPI/Models/Job.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 namespace JobSearchingAPI.Models
 {
     public class Job
-    {
+    {   public int JobId { get; set; }
         public string skillSet { get; set; }
         public string jobTitle { get; set; }
         public string jobSalary { get; set; }
-        public string compnay { get; set; }
+        public string company { get; set; }
         public string WorkHours { get; set; }
         public string location { get; set; }
         public string jobDescription { get; set; }
